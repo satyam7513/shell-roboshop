7a95de82c5132438f7ed6750b79bc4642d87454b
satyam7513 <satyamknaidu@gmail.com>
2025-11-04 12:07:03 +0530

    roboshop-shell

--- roboshop.sh
+++ roboshop.sh
@@ -10,14 +10,10 @@ DOMAIN_NAME:"kimidi.site"
 
 for instance in ${INSTANCES[@]}
 do
-    INSTANCE_ID=$(aws ec2 run-instances \
-     --image-id ami-09c813fb71547fc4f \
-     --instance-type t2.micro \
-     --security-group-ids sg-02848a76f76cb5410 \
-     --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=test}]' \
-     --query "Instances[0].InstanceId" \
-     --output text)
+     INSTANCE_ID=$(aws ec2 run-instances --image-id ami-09c813fb71547fc4f --instance-type t3.micro --security-group-ids sg-02848a76f76cb5410 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
     if [ $instance != "frontend" ]
+   
+    
     then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
       
