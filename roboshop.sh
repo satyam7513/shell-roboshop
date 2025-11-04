bcd27397442fa39babb8dcd46f61b288beab198a
satyam7513 <satyamknaidu@gmail.com>
2025-11-04 11:29:53 +0530

    instance create

---
+++ roboshop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+
+AMI_ID="ami-09c813fb71547fc4f"
+SG_ID="sg-02848a76f76cb5410"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+zone_ID="Z08366282S5RRXUDFM0PY"
+DOMAIN_NAME:"kimidi.site"
+
+for instance in ${INSTANCES[@]}
+do
+
+done 
