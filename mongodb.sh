3d7527e2995567689384692da8ef1d471d09b958
satyam7513 <satyamknaidu@gmail.com>
2025-11-05 16:26:22 +0530

    roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -23,7 +23,7 @@ fi
 
 #validate functions takes input as exit status,what command they tried to install
 VALIDATE(){
-    if [ $1 -eq 0]
+    if [ $1 -eq 0 ]
     then
        echo -e "$2 is ...$G SUCCESS $N" | tee -a $LOG_FILE
     else
