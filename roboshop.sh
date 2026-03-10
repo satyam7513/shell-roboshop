4e41c5f21b98baeb2694e43c728178732a039cf8
satyam7513 <satyamknaidu@gmail.com>
2026-03-10 19:44:51 +0530

    Removed unused service scripts and updated roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -1,45 +1,25 @@
 #!/bin/bash
 
-AMI_ID="ami-09c813fb71547fc4f"
-SG_ID="sg-02848a76f76cb5410"
-ZONE_ID="Z08366282S5RRXUDFM0PY"
+ami-id="ami-0220d79f3f480ecf5"
+sg_id="sg-02b2c0c8175091ab8"
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "frontend")
+ZONE_ID="Z04626924Z2WBLM8PDBQ"
 DOMAIN_NAME="kimidi.site"
 
-# Loop through all instances passed as arguments
-for instance in "$@"
-do
-    echo "Launching instance: $instance"
-    INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id "$AMI_ID" \
-        --instance-type t3.micro \
-        --security-group-ids "$SG_ID" \
-        --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-        --query "Instances[0].InstanceId" --output text)
 
-    if [ "$instance" != "frontend" ]; then
-        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" \
-             --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
+for instance in ${INSTANCES[@]}
+#for instance in $@
+do
+    INSTANCE_ID=$(aws ec2 run-instances --image-id ami-0220d79f3f480ecf5 --instance-type t3.micro --security-group-ids sg-02b2c0c8175091ab8 --tag-specifications "ResourceType=instance,Tags=[{Key=Name, Value=$instance}]" --query "Instances[0].InstanceId" --output text)
+    if [ $instance != "frontend" ]
+    then
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PrivateIpAddress" --output text)
         RECORD_NAME="$instance.$DOMAIN_NAME"
     else
-        IP=$(aws ec2 describe-instances --instance-ids "$INSTANCE_ID" \
-             --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query "Reservations[0].Instances[0].PublicIpAddress" --output text)
         RECORD_NAME="$DOMAIN_NAME"
     fi
-
     echo "$instance IP address: $IP"
 
-    aws route53 change-resource-record-sets \
-      --hosted-zone-id "$ZONE_ID" \
-      --change-batch "{
-        \"Comment\": \"Creating or Updating a record set for $instance\",
-        \"Changes\": [{
-          \"Action\": \"UPSERT\",
-          \"ResourceRecordSet\": {
-            \"Name\": \"$RECORD_NAME\",
-            \"Type\": \"A\",
-            \"TTL\": 300,
-            \"ResourceRecords\": [{\"Value\": \"$IP\"}]
-          }
-        }]
-      }"
-done
+
+
