d8f8213dc11c19ed822986897ce788e645a284e2
satyam7513 <satyamknaidu@gmail.com>
2026-03-13 19:56:26 +0530

    catalogue

--- catalogue.sh
+++ catalogue.sh
@@ -78,11 +78,11 @@ cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo
 dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Installing MongoDB Client"
 
-#STATUS=$(mongosh --host mongodb.somaraju.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
-#if [ $STATUS -lt 0 ]
-#then
+STATUS=$(mongosh --host mongodb.somaraju.online --eval 'db.getMongo().getDBNames().indexOf("catalogue")')
+if [ $STATUS -lt 0 ]
+then
     mongosh --host mongodb.somaraju.online </app/db/master-data.js &>>$LOG_FILE
     VALIDATE $? "Loading data into MongoDB"
-#else
-#    echo -e "Data is already loaded ... $Y SKIPPING $N"
-#fi
+else
+    echo -e "Data is already loaded ... $Y SKIPPING $N"
+fi
