0fcb7eb48b74c8d1fdbb2a63292ebff4f48a5099
satyam7513 <satyamknaidu@gmail.com>
2026-03-14 11:54:17 +0530

    shipping

---
+++ shipping.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+
+START_TIME=$(date +%s)
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+# check the user has root priveleges or not
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access" | tee -a $LOG_FILE
+fi
+
+echo "Please enter root password to setup"
+read -s MYSQL_ROOT_PASSWORD
+
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
+    else
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+
+dnf install maven -y
+VALIDATE $? "maven Installing & Java"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+   VALIDATE $? "Roboshop user adding"
+
+else
+   echo -e "Sytem user Roboshop already created.. $Y SKIPPING /N"
+fi
+
+mkdir -p /app
+VALIDATE $? "creating app directory"
+
+curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping-v3.zip
+VALIDATE $? "Downloding shipping"
+
+rm -rf /app/*
+cd /app 
+unzip /tmp/shipping.zip
+VALIDATE $? "unzipping shipping"
+
+mvn clean package 
+VALIDATE $? "packagin the shipping application"
+
+mv target/shipping-1.0.jar shipping.jar 
+VALIDATE $? "moving and renaming the jar file"
+
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+VALIDATE "Copying shipping.service"
+
+systemctl daemon-reload
+VALIDATE $? "Daemon reloading"
+
+systemctl enable shipping 
+VALIDATE $? "Enabling shipping"
+
+systemctl start shipping
+VALIDATE $? "Starting shipping"
+
+dnf install mysql -y
+VALIDATE $? "Installing MYSQL"
+
+mysql -h mysql.kimidi.site -u root -p$MYSQL_ROOT_PASSWORD -e 'use cities'
+if [ $? -ne 0 ]
+then
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/app-user.sql  &>>$LOG_FILE
+    mysql -h mysql.daws84s.site -uroot -p$MYSQL_ROOT_PASSWORD < /app/db/master-data.sql &>>$LOG_FILE
+    VALIDATE $? "Loading data into MySQL"
+else
+    echo -e "Data is already loaded into MySQL ... $Y SKIPPING $N"
+fi
+
+systemctl restart shipping
+VALIDATE $? "Restart shipping"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script Exicution completed successfully.. $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
+
+
+
+
+
+
+
+
+
